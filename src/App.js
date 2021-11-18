636a945c3c24b54a5b5601cbe8f292c54bc53d9a
adisekar <adithya.kiran.sekar@gmail.com>
2021-11-18 11:41:55 -0500

    Updated docker dev and compose

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Helloo There!
+          Hey There!
         </p>
         <a
           className="App-link"
